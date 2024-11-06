f498687f6971670a5d38e022e3e3bbd8cba9db49
Askarbek Almazbek uulu <almazuulu@gmail.com>
2024-11-06 12:23:46 +0800

    Routers configurations

---
+++ shop/urls.py
@@ -0,0 +1,31 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import (
+    # ViewSets
+    CategoryViewSet,
+    ManufacturerViewSet,
+    ProductViewSet,
+    CustomerViewSet,
+    
+    # Views - TODO: Implement it later
+    # WebSocketTestView,
+)
+
+app_name = 'shop'
+
+# REST API Routers settinsg
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'manufacturers', ManufacturerViewSet, basename='manufacturer')
+router.register(r'products', ProductViewSet, basename='product')
+router.register(r'customers', CustomerViewSet, basename='customer')
+
+# URL patterns shops
+urlpatterns = [
+    # REST API URLs
+    path('api/', include(router.urls)),
+    
+    # WebSocket test page
+    # TODO: Implement later
+    # path('ws-test/', WebSocketTestView.as_view(), name='ws-test'),
+]
